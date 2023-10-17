2a1c9df9a3fb972488da9e417c49b024c30c525d
eugenekhoo <ktw1995@gmail.com>
2023-10-17 11:17:11 +0800

    update cors setting

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5000", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5000",
+  "http://localhost:3000",
+  "https://weth-usdc-v3.vercel.app/",
+];
 
 module.exports = allowedOrigins;
